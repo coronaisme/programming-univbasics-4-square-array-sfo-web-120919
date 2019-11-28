7e85e48273c79dd1f965d5831128df6933535428
Nick Corona <coronaisme@gmail.com>
2019-11-28 07:58:55 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,9 @@
 def square_array(array)
 counter = 0
+new_array = []
     while counter < array.length do
-    arraarray[counter] ** 2
+    new_array.push(array[counter] ** 2)
     counter += 1
 end
+return new_array
 end
